afa9e479d508ca35bce76355272445814a4cf6e1
TrongLe1 <quoctrongle2001@gmail.com>
2021-11-09 21:38:37 +0700

    Coding account linked to google

--- src/ChillnFree/routes/account.js
+++ src/ChillnFree/routes/account.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+// Google Auth
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '587388834334-fb1iddt85o3r4vlf77h80u745d4d2l2d.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
+
 /* GET users listing. */
 router.get('/signIn', function(req, res, next) {
   res.render("signin",{title: "ChillnFree - Sign In"});
 });
 router.get('/signUp', function(req, res, next) {
     res.render("signup",{title: "ChillnFree - Sign Up"});
-  });
+ });
+
+ router.post("/login", function (req, res, next) {
+  let token = req.body.token;
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+
+  }
+  verify().then(()=>{
+    res.cookie('session-token', token);
+    res.send('success');
+  }).catch(console.error);
+ 
+});
+
+router.get("/logout", function (req, res, next) {
+  res.clearCookie('session-token');
+  res.redirect('/')
+});
+
 
 module.exports = router;
--- src/ChillnFree/routes/home_page.js
+++ src/ChillnFree/routes/home_page.js
@@ -2,17 +2,46 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '587388834334-fb1iddt85o3r4vlf77h80u745d4d2l2d.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
 
 router.get("/", function (req, res, next) {
   res.render("home_page", { title: "ChillnFree" });
 });
 
-router.get("/home_signedin", function (req, res, next) {
+router.get("/home_signedin", SignedInAllowed, function (req, res, next) {
   // console.log('reached the home signed in route');
+  let user = req.user;
+  //let token = req.cookies['session-token'];
   res.render("home_signedin", {
     title: "ChillnFree",
-    nameOfAccount: "Account",
+    nameOfAccount: user.name,
   });
 });
+
+function SignedInAllowed(req, res, next){
+  let token = req.cookies['session-token'];
+  let user = {};
+  async function verify() {
+      const ticket = await client.verifyIdToken({
+          idToken: token,
+          audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      });
+      const payload = ticket.getPayload();
+      user.name = payload.name;
+      user.email = payload.email;
+      user.picture = payload.picture;
+    }
+  verify()
+  .then(()=>{
+      req.user = user;
+      next();
+  })
+  .catch(err=>{
+      res.redirect('/account/signIn')
+  })
+
+}
 
 module.exports = router;
