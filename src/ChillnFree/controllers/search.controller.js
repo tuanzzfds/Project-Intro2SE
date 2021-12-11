890294de17478514dc107ad50983f5ff5dcf7c8b
TrongLe1 <quoctrongle2001@gmail.com>
2021-12-11 16:47:25 +0700

    Update search function and view

--- src/ChillnFree/controllers/search.controller.js
+++ src/ChillnFree/controllers/search.controller.js
@@ -2,6 +2,6 @@ const Song = require('../models/songs.model')
 
 
 module.exports.func = async function (keyword) { 
-    return await Song.find({$or:[{title: {$regex: '.*' + keyword + '.*'}},{artist: {$regex: '.*' + keyword + '.*'}},{genre: {$regex: '.*' + keyword + '.*'}}]});
+    return await Song.find({$or:[{title: {$regex:keyword,$options:'i'}},{artist: {$regex:keyword ,$options:'i'}},{genre: {$regex:keyword,$options:'i'}}]});
 };
 
